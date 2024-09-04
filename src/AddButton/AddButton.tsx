efee749dbff54764186d06daa4a6c24472464667
Saki Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2024-09-04 09:00:23 -0600

    Create add to cart button

---
+++ src/AddButton/AddButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AddButton.css';
+
+const AddButton = () => {
+    return (
+        <button className="add-button">Add to cart</button>
+    )
+}
+
+export default AddButton
