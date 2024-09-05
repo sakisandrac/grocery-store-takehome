8e7dc33a9cd76b7f8a10fdaadeca635a73673426
Saki Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2024-09-04 18:21:55 -0600

    Replace shape data function

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Homepage from '../Homepage/Homepage';
 import Navigation from '../Navigation/Navigation';
-import { FoodItem, Hint } from '../types';
+import { FoodItem } from '../types';
 import { Route, Routes } from 'react-router-dom';
 import Checkout from '../Checkout/Checkout';
 import Search from '../Search/Search';
 import ErrorPage from '../ErrorPage/ErrorPage';
+import { API_ID, API_KEY, shapeFoodData } from '../utlities';
 
 const App = () => {
   const [data, setData] = useState<FoodItem[]>([]);
@@ -14,13 +15,11 @@ const App = () => {
   const [cartItems, setCartItems] = useState<FoodItem[]>([]);
   const [error, setError] = useState('');
 
-  const APP_ID = process.env.REACT_APP_API_ID;
-  const APP_KEY = process.env.REACT_APP_API_KEY;
   const query = ['generic-foods'];
 
   const fetchFoodData = async (query: string[]) => {
     try {
-      const response = await fetch(`https://api.edamam.com/api/food-database/v2/parser?category=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const response = await fetch(`https://api.edamam.com/api/food-database/v2/parser?category=${query}&app_id=${API_ID}&app_key=${API_KEY}`);
 
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -28,15 +27,7 @@ const App = () => {
 
       const data = await response.json();
 
-      const shapedData: FoodItem[] = data.hints.map((item: Hint) => ({
-        foodId: item.food.foodId,
-        label: item.food.label,
-        brand: item.food.brand,
-        category: item.food.category,
-        image: item.food.image
-      }));
-
-      setData(shapedData);
+      setData(shapeFoodData(data));
     } catch (error) {
       setError('Failed to fetch data');
     }
