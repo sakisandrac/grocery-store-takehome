98a49f5327ee4cb30ade15dbb58e9c9117431e05
Saki Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2024-09-04 21:23:30 -0600

    Add form to checkout

--- src/Checkout/Checkout.tsx
+++ src/Checkout/Checkout.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { CartQuantity, FoodItem } from '../types';
+import { CartQuantity, FoodItem, FormData } from '../types';
 import './Checkout.css';
 import { countItemsInCart } from '../utlities';
 import thankYou from '../resources/thank-you.png';
+import Form from '../Form/Form';
+import shipping from '../resources/shipping.png';
 
 interface CheckoutProps {
     cartItems: FoodItem[]
@@ -15,6 +17,30 @@ const Checkout = ({ cartItems, setCartItems }: CheckoutProps) => {
     const cartSummary = countItemsInCart(cartItems);
     const totalQuantity = cartSummary.reduce((acc, item) => acc + item.quantity, 0);
     const isCartEmpty = totalQuantity === 0;
+
+    const [formData, setFormData] = useState<FormData>({
+        name: '',
+        email: '',
+        streetAddress: '',
+        city: '',
+        state: '',
+        paymentMethod: 'cash',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const submitPurchase = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setPurchasedItems(cartSummary);
+        setFinishPurchase(true);
+        setCartItems([]);
+    }
 
     return (
         <div className="checkout-main">
@@ -34,32 +60,33 @@ const Checkout = ({ cartItems, setCartItems }: CheckoutProps) => {
                     ))}
                 </div>
                 <img className="checkout-image" src={thankYou} alt="groceries and thank you text" />
-            </div>) : <div className="checkout-review">
-                <h1>Review Your Order</h1>
-                {isCartEmpty ? <p>There are no items in your cart!</p> :
-                    (
-                        <div className="checkout-items-container">
-                            <div className="checkout-items">
-                                {cartSummary?.map((item: CartQuantity, index: number) => (
-                                    <div key={index} className="checkout-item">
-                                        {item.item.image && (
-                                            <>
-                                                <img src={item.item.image} alt={item.item.label} className="checkout-item-image" />
-                                                <p>{item.item.label}</p>
-                                                <p>Quantity: {item.quantity}</p>
-                                            </>
-                                        )}
-                                    </div>
-                                ))}
+            </div>) :
+                <div className="checkout-review">
+                    <h1>Review Your Order</h1>
+                    {isCartEmpty ? <p>There are no items in your cart!</p> :
+                        (<div className="checkout-review-container">
+                            <div className="checkout-items-container">
+                                <div className="checkout-items">
+                                    {cartSummary?.map((item: CartQuantity, index: number) => (
+                                        <div key={index} className="checkout-item">
+                                            {item.item.image && (
+                                                <>
+                                                    <img src={item.item.image} alt={item.item.label} className="checkout-item-image" />
+                                                    <p>{item.item.label}</p>
+                                                    <p>Quantity: {item.quantity}</p>
+                                                </>
+                                            )}
+                                        </div>
+                                    ))}
+                                </div>
+                                <p>Total number of items: {totalQuantity}</p>
                             </div>
-                            <p>Total number of items: {totalQuantity}</p>
-                            <button className="checkout-btn" onClick={() => {
-                                setPurchasedItems(cartSummary)
-                                setCartItems([])
-                                setFinishPurchase(prev => !prev)
-                            }}>Purchase</button>
+                            <div className='checkout-form-container'>
+                                <img src={shipping} alt="enter shipping info" className="checkout-item-image" />
+                                <Form formData={formData} handleChange={handleChange} submitPurchase={submitPurchase} />
+                            </div>
                         </div>)}
-            </div>}
+                </div>}
         </div>
     )
 }
